8dc8fdb7e12e587d0db027e3fe45d7b35a7151f6
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-01-09 21:28:45 +0800

    Update ins_pkg.sh

--- ins_pkg.sh
+++ ins_pkg.sh
@@ -10,7 +10,7 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 alpine_pkgs="bash curl gcc git jq libffi-dev make musl-dev openssl-dev perl perl-app-cpanminus perl-dev py3-pip python3 python3-dev wget"
 py_reqs="bs4 cryptography pyaes pyppeteer requests rsa schedule tomli"
-js_pkgs="@iarna/toml axios crypto-js got"
+js_pkgs="@iarna/toml axios crypto-js got qs"
 pl_mods="File::Slurp JSON5 TOML::Dumper"
 
 install() {

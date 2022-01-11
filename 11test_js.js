faca09c069e149e43368ea6ce1bda237c07918e9
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-01-11 13:43:58 +0800

    Delete 11test_js.js

--- 11test_js.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const qs = require('qs');
-
-const utils = require('./utils');
-const Env = utils.Env;
-const getData = utils.getData;
-
-const $ = new Env('test_js');
-const COOKIES_TESTJS = getData().TESTJS;

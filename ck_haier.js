7a05ab4383a44e7e8a2f7612c2eacab5ac8bfead
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-01-27 15:12:00 +0800

    Update ck_haier.js

--- ck_haier.js
+++ ck_haier.js
@@ -23,7 +23,7 @@ function haier() {
     if (AsVow) {
         for (i in AsVow) {
             token = AsVow[i].token;
-            head = `=== 正对在第1个账号签到===\n`;
+            head = `=== 正对在第 ${i+1} 个账号签到===\n`;
             info += `\n${head}`;
             url = 'https://113.16.212.181/zjapi/zjBaseServer/daily/sign';
             headers['accountToken'] = token;
@@ -38,13 +38,13 @@ function haier() {
             })
             .then(function(body) {
                 if (body.retInfo.includes('成功')) {
-		            if (body.data.first) {
-	                   info += `${body.data.signDate}首次签到成功\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
-		            } else {
-	                   info += `${body.data.signDate}已签到过啦~\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
-		            }
-		            url = 'https://113.16.212.181/zjapi/zjBaseServer/signDetail/getUserPointsAndWallet';
-		            fetch(url, {
+                    if (body.data.first) {
+                       info += `${body.data.signDate}首次签到成功\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
+                    } else {
+                       info += `${body.data.signDate}已签到过啦~\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
+                    }
+                    url = 'https://113.16.212.181/zjapi/zjBaseServer/signDetail/getUserPointsAndWallet';
+                    fetch(url, {
                         method: 'POST',
                         headers: headers,
                         body: JSON.stringify(body)
@@ -52,29 +52,31 @@ function haier() {
                     .then(function(response) {
                         return response.json()
                     })
-		            .then(function(body) {
-		                if (body.retInfo.includes('成功')) {
-		                   info += `共计持有海贝 ${body.data.haibeiTotal} 个\n`;
-		                   console.log(info);
-		                } else {
+                    .then(function(body) {
+                        if (body.retInfo.includes('成功')) {
+                           info += `共计持有海贝 ${body.data.haiBeiTotal} 个\n`;
+                           console.log(info);
+                        } else {
                             info += "查询钱包信息返回错误，请重新调试";
+                            console.log(info);
                             notify.sendNotify('海尔', info);
                         }
-		            })
-		            .catch(function(e) {
-	                    info += "获取钱包信息失败" + e;
-		                console.log(info);
-		                notify.sendNotify('海尔', info);
-	                });
-	            } else {
+                    })
+                    .catch(function(e) {
+                        info += "获取钱包信息失败" + e;
+                        console.log(info);
+                        notify.sendNotify('海尔', info);
+                    });
+                } else {
                     info += "签到返回错误，请重新调试";
+                    console.log(info);
                     notify.sendNotify('海尔', info);
                 }
             })
             .catch(function(e) {
-              info += "签到失败" + e;
-              console.log(info);
-              notify.sendNotify('海尔', info);
+               info += "签到失败" + e;
+               console.log(info);
+               notify.sendNotify('海尔', info);
             });
         }
     }

8b776c5cc0915986895268bc04471f936b793ae4
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-01-29 22:49:43 +0800

    Update ck_haier.js

--- ck_haier.js
+++ ck_haier.js
@@ -7,8 +7,8 @@ const Env = utils.Env;
 const $ = new Env('海尔');
 const getData = utils.getData;
 const notify = $.isNode() ? require('./sendNotify') : '';
-
 const AsVow = getData().HAIER;
+var desp = '';
 var info = '';
 
 const headers = {
@@ -19,68 +19,78 @@ const headers = {
 
 haier();
 
-function haier() {
+async function haier() {
     if (AsVow) {
         for (i in AsVow) {
             token = AsVow[i].token;
-            head = `=== 正对在第 ${i+1} 个账号签到===\n`;
-            info += `\n${head}`;
-            url = 'https://113.16.212.181/zjapi/zjBaseServer/daily/sign';
-            headers['accountToken'] = token;
-            body = {};
-            fetch(url, {
-              method: 'POST',
-              headers: headers,
-              body: JSON.stringify(body)
-            })
-            .then(function(response) {
-              return response.json()
-            })
-            .then(function(body) {
-                if (body.retInfo.includes('成功')) {
-                    if (body.data.first) {
-                       info += `${body.data.signDate}首次签到成功\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
-                    } else {
-                       info += `${body.data.signDate}已签到过啦~\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
-                    }
-                    url = 'https://113.16.212.181/zjapi/zjBaseServer/signDetail/getUserPointsAndWallet';
-                    fetch(url, {
-                        method: 'POST',
-                        headers: headers,
-                        body: JSON.stringify(body)
-                    })
-                    .then(function(response) {
-                        return response.json()
-                    })
-                    .then(function(body) {
-                        if (body.retInfo.includes('成功')) {
-                           info += `共计持有海贝 ${body.data.haiBeiTotal} 个\n`;
-                           console.log(info);
-                           notify.sendNotify('海尔', info);
-                        } else {
-                            info += "查询钱包信息返回错误，请重新调试";
-                            console.log(info);
-                            notify.sendNotify('海尔', info);
-                        }
-                    })
-                    .catch(function(e) {
-                        info += "获取钱包信息失败" + e;
-                        console.log(info);
-                        notify.sendNotify('海尔', info);
-                    });
-                } else {
-                    info += "签到返回错误，请重新调试";
-                    console.log(info);
-                    notify.sendNotify('海尔', info);
-                }
-            })
-            .catch(function(e) {
-               info += "签到失败" + e;
-               console.log(info);
-               notify.sendNotify('海尔', info);
-            });
+            info +=`=== 正对在第 ${i+1} 个账号签到===\n`;
+            await sign();
+            await getinfo();
         }
+        console.log(info);
+        notify.sendNotify('海尔', info);
+    } else {
+        info = '签到失败：请先获取Cookie⚠️';
+        notify.sendNotify('海尔', info);
     }
+}
+
+function sign() {
+    url = 'https://zj.haier.net/zjapi/zjBaseServer/daily/sign';
+    headers['accountToken'] = token;
+    body = {};
+    return new Promise(resolve => {
+      fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+      }).then(function(response) {
+        return response.json()
+      }).then(function(body) {
+        if (body.retInfo.includes('成功')) {
+            if (body.data.first) {
+              info += `${body.data.signDate}首次签到成功\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
+            } else {
+              info += `${body.data.signDate}已签到过啦~\n获得 ${body.data.haibeiCount} 个海贝\n连续签到 ${body.data.signDay} 天\n总计签到 ${body.data.totalSignDay} 天\n`;
+            }
+        } else {
+             info += "签到返回错误，请重新调试";
+             console.log(info);
+        }
+  
+      }).catch(function(e) {
+          const error = '签到出现错误，请检查⚠️';
+          console.log(error + '\n' + e);
+      }).finally(() => {
+          resolve()
+      })
+    });
+}
+
+function getinfo() {
+    url = 'https://zj.haier.net/zjapi/zjBaseServer/signDetail/getUserPointsAndWallet';
+    headers['accountToken'] = token;
+    body = {};
+    return new Promise(resolve => {
+      fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+      .then(function(response) {
+        return response.json()
+      }).then(function(body) {
+        if (body.retInfo.includes('成功')) {
+            info += `共计持有海贝 ${body.data.haiBeiTotal} 个\n`;
+        }  else {
+            info += "查询钱包信息返回错误，请重新调试";
+        }
+      }).catch(function(e) {
+          info += "获取钱包信息失败" + e;
+      }).finally(() => {
+          resolve()
+      })
+    });
 }
 
 module.exports = haier;

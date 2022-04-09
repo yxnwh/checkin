fb95563d8c6d050b4bf4594908d5d879b6fecd9f
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-04-09 12:23:29 +0800

    Update ck_didi.py

--- ck_didi.py
+++ ck_didi.py
@@ -25,7 +25,7 @@ class DIDI:
 
     def get_xpsid(self):
         try:
-            url = f'https://v.didi.cn/p/DpzAd35?appid=10000&lang=zh-CN&clientType=1&trip_cityid=21&datatype=101&imei=99d8f16bacaef4eef6c151bcdfa095f0&channel=102&appversion=6.2.4&trip_country=CN&TripCountry=CN&lng=113.812538&maptype=soso&os=iOS&utc_offset=480&access_key_id=1&deviceid=99d8f16bacaef4eef6c151bcdfa095f0&phone=UCvMSok42+5+tfafkxMn+A==&model=iPhone11&lat=23.016271&origin_id=1&client_type=1&terminal_id=1&sig=8503d986c0349e40ea10ff360f75d208c78c989a'
+            url = f'https://v.didi.cn/p/DpzAd35'
             heards = {
                 "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
             }

e95d698ac574282fcb2bb0fa37d801a3620175f8
yxnwh <36069159+yxnwh@users.noreply.github.com>
2022-04-13 11:02:02 +0800

    Update ck_didi.py

--- ck_didi.py
+++ ck_didi.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-# @url: https://raw.githubusercontent.com/zyf1118/kevinxf/main/xF_didi_Sign.py
 # @author: zyf1118
 # https://gsh5act.xiaojukeji.com/dpub_data_api/activities/9612/signin?signin_user_token=xxxx中的signin_user_token
-# https://dpubstatic.udache.com/static/dpubimg/dpub2_project_xxxxx/index_xxxxx.json?r=0.6781450893324913?ts=1637987237802&app_id=common，查看josn里面的activity_id
 # 特别说明：本脚本需要从星期一签到，作为第一天签到，如果今天是星期二，你之前没签到过，那脚本是不能签到的。
 """
 cron: 48 8 * * *
@@ -14,6 +12,7 @@ import time,datetime
 
 from notify_mtr import send
 from utils import get_data
+from hashids import Hashids
 
 requests.packages.urllib3.disable_warnings()
 
@@ -27,41 +26,22 @@ class DIDI:
         try:
             url = f'https://v.didi.cn/p/DpzAd35'
             heards = {
-                "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
             }
             response = requests.head (url=url, headers=heards, verify=False)  # 获取响应请求头
             res = response.headers['Location']  # 获取响应请求头
-            # print(res)
             r = re.compile (r'root_xpsid=(.*?)&channel_id')
             xpsid = r.findall (res)[0].split("&")[0]
             return xpsid
         except Exception as e:
             print (e)
     
-    #获取v.didi.cn的url
-    @staticmethod
-    def get_v_url(token):
-        nowtime = int (round (time.time () * 1000))
-        url = f'https://common.diditaxi.com.cn/common/v5/usercenter/me?_t={nowtime}&access_key_id=1&appversion=6.2.4&channel=102&city_id=21&datatype=101&imei=99d8f16bacaef4eef6c151bcdfa095f0&lang=zh-CN&maptype=soso&model=iPhone&networkType=WIFI&os=15.0&sig=c783e6e425a59349309ad10a4c1843a54fc9e82c&terminal_id=1&token={token}&v6x_version=1'
-        heards = {
-            "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
-        }
-        response = requests.get (url=url, headers=heards, verify=False)    #获取响应请求头
-        result = response.json()                                       #获取响应请求头
-        bottom_items = result['data']['cards'][3]['bottom_items']
-        for i in range(len(bottom_items)):
-            title = bottom_items[i]['title']
-            if "积分商城" in title:
-                link = bottom_items[i]['link']
-                v_url = link[18:]
-        return v_url
-    
     #获取s.didi.cn的url
     def get_s_url(self):
         nowtime = int (round (time.time () * 1000))
         url = f'https://v.didi.cn/K0gkogR'
         heards = {
-            "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+            "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
         }
         response = requests.head (url=url, headers=heards, verify=False)            #获取响应请求头
         result = response.headers['Location']                                       #获取响应请求头
@@ -72,13 +52,12 @@ class DIDI:
     #获取url
     @staticmethod
     def get_url(s_url):
-        url = f'https://s.didi.cn/{s_url}?channel_id=72%2C278%2C80537&entrance_channel=7227880537&xsc=&dchn=K0gkogR&prod_key=custom&xbiz=&xpsid=cc2e4bc570d74253ad56b6c927473c0d&xenv=passenger&xspm_from=&xpsid_from=&xpsid_root=cc2e4bc570d74253ad56b6c927473c0d'
+        url = f'https://s.didi.cn/{s_url}'
         heards = {
-            "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+            "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
         }
         response = requests.head (url=url, headers=heards, verify=False)    #获取响应请求头
         res = response.headers['location']                                  #获取响应请求头
-        # print(res)
         r = re.compile (r'dpubimg/(.*?)/index.html', re.M | re.S | re.I)
         url_id = r.findall (res)
         url_id = url_id[0]
@@ -91,9 +70,9 @@ class DIDI:
             day = time.localtime ()
             day = time.strftime ("%w", day)  # 今天星期几，0代表星期天
             day = int (day)
-            url = f'https://dpubstatic.udache.com/static/dpubimg/{url_id}/index.html?channel_id=72%2C278%2C80537&dchn=K0gkogR&entrance_channel=7227880537&prod_key=custom&xbiz=&xenv=passenger&xpsid=9ef4ad1c8e3d42fab6d9823bc4f9838b&xpsid_from=&xpsid_root=9ef4ad1c8e3d42fab6d9823bc4f9838b&xsc=&xspm_from='
+            url = f'https://dpubstatic.udache.com/static/dpubimg/{url_id}/index.html'
             heards = {
-                "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
                 "Referer": "https://page.udache.com/",
                 "Host": "dpubstatic.udache.com",
                 "Origin": "https://dpubstatic.udache.com",
@@ -130,24 +109,23 @@ class DIDI:
             print (e)
 
     
-    #获取个人信息
+    #执行签到
     @staticmethod
-    def get_activity_info(token,day,numb):
+    def do_sign(token,day,numb):
         try:
             do_sign_url = f'https://gsh5act.xiaojukeji.com/dpub_data_api/activities/{numb}/signin'
             data = r'{"signin_day":' + f"{day}" + r',"signin_type":0,"signin_user_token":' + '"' + f'{token}' + r'"}'
             do_sign_heards = {
-                "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
                 "Referer": "https://dpubstatic.udache.com/",
                 "Host": "gsh5act.xiaojukeji.com",
                 "Origin": "https://dpubstatic.udache.com",
                 "Content-Type": "application/json",
             }
             response = requests.post (url=do_sign_url, headers=do_sign_heards, data=data, verify=False)
-            do_sign_ = response.json ()
-            print ("do_sign_#获取个人信息")
-            print (do_sign_)
-            code = do_sign_['errmsg']
+            result = response.json ()
+            print ("do_sign#执行签到\n"+f'{result}')
+            code = result['errmsg']
             if "已结束" in code:
                 res = f"获取签到ID异常"
             elif "已经" in code:
@@ -166,10 +144,9 @@ class DIDI:
     def reward(token,day,numb,id):
         try:
             while True:
-                nowtime = int (round (time.time () * 1000))
                 info_url = f'https://gsh5act.xiaojukeji.com/dpub_data_api/activities/{numb}/reward_lottery'
                 info_headers = {
-                    "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                    "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
                     "Referer": "https://dpubstatic.udache.com",
                     "Host": "gsh5act.xiaojukeji.com",
                     "Origin": "https://dpubstatic.udache.com",
@@ -178,8 +155,7 @@ class DIDI:
                 data = '{'+ f'"user_token":"{token}","signin_day":{day},"lottery_id":"{id}"' +'}'
                 response = requests.post(url=info_url, headers=info_headers, verify=False,data=data)
                 list = response.json()
-                print ("reward#获取积分")
-                print(list)
+                print ("reward#获取积分\n"+f'{list}')
                 flag = list['errmsg']
                 if "签到当天奖励" in flag:
                     res = f"已经领过签到当天奖励"
@@ -198,21 +174,46 @@ class DIDI:
             print (e)
 
    
-    #获取抽奖lid
-    def get_lid(self):
+    @staticmethod
+    def get_s(xpsid):
         try:
-            nowtime = int (round (time.time () * 1000))     #13位
-            info_url = f'https://dpubstatic.udache.com/static/dpubimg/dpub2_project_1275480/index_VYom0.json?r=0.07526362250404772?ts={nowtime}&app_id=common'
+            info_url = f'https://api.didi.cn/webx/chapter/cover/config'
+            data = r'{"xbiz":"110000","prod_key":"custom","xpsid":"' + f'{xpsid}' + r'","dchn":"nR9avq9","xoid":"LH7idOwBTVCmNDQccI2elg","uid":"","xenv":"passenger","xspm_from":"ut-mall.none.c605.2","xpsid_root":"' + f'{xpsid}' + r'","xpsid_from":"","xpsid_share":"","args":{"dchn":"nR9avq9","xenv":"passenger","pass_through":"xenv=passenger"},"root_xpsid":"' + f'{xpsid}' + r'","f_xpsid":"' + f'{xpsid}' + r'"}'
+            info_heards = {
+                "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                "Referer": "https://page.udache.com/",
+                "Host": "api.didi.cn",
+                "Origin": "https://page.udache.com",
+                "Content-Type": "application/json",
+            }
+            response = requests.post (url=info_url, headers=info_heards, data=data, verify=False)
+            result = response.json ()
+            print ("get_s#获取s字段\n"+f'{result}')
+            code = result['errmsg']
+            if "success" in code:
+                s_url = result['data']['conf']['url']
+                r = re.compile (r's=(.*?)&dchn')
+                s = r.findall (s_url)[0]
+                return s
+            else:
+                print (result)
+        except Exception as e:
+            print (e)
+           
+    #获取抽奖lid
+    @staticmethod
+    def get_lid(hashid,sid):
+        try:
+            info_url = f'https://dpubstatic.udache.com/static/dpubimg/dpub2_project_{hashid}/index_{sid}.json'
             info_headers = {
-                "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
                 "Referer": "https://page.udache.com/",
                 "Host": "dpubstatic.udache.com",
                 "Origin": "https://page.udache.com",
             }
             response = requests.get(url=info_url, headers=info_headers, verify=False)
             result = response.json()
-            print ("get_lid#获取抽奖lid")
-            print(result)
+            print ("get_lid#获取抽奖lid\n"+f'{result}')
             activity_id = result['activity_id']
             return activity_id
         except Exception as e:
@@ -225,17 +226,16 @@ class DIDI:
         try:
             flag = 6
             while True:
-                do_Lottery_url = f'https://bosp-api.xiaojukeji.com/bosp-api/lottery/draw?lid={lottery_lid}&token={token}&env=%7B%22longitude%22%3A113.81251003689236%2C%22latitude%22%3A23.016395128038194%2C%22cityId%22%3A%2221%22%2C%22deviceId%22%3A%2299d8f16bacaef4eef6c151bcdfa095f0%22%2C%22ddfp%22%3A%2299d8f16bacaef4eef6c151bcdfa095f0%22%2C%22appVersion%22%3A%226.2.4%22%2C%22wifi%22%3A1%2C%22model%22%3A%22iPhone%2011%22%2C%22timeCost%22%3A637425%2C%22userAgent%22%3A%22Mozilla%2F5.0%20(iPhone%3B%20CPU%20iPhone%20OS%2015_0%20like%20Mac%20OS%20X)%20AppleWebKit%2F605.1.15%20(KHTML%2C%20like%20Gecko)%20Mobile%2F15E148%20didi.passenger%2F6.2.4%20FusionKit%2F1.2.20%20OffMode%2F0%22%2C%22isHitButton%22%3Atrue%7D'
+                do_Lottery_url = f'https://bosp-api.xiaojukeji.com/bosp-api/lottery/draw?lid={lottery_lid}&token={token}'
                 do_Lottery_headers = {
-                    "user-agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
+                     "User-Agent": f"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 didi.passenger/6.2.4 FusionKit/1.2.20 OffMode/0",
                     "Referer": "https://page.udache.com/",
                     "Host": "bosp-api.xiaojukeji.com",
                     "Origin": "https://page.udache.com",
                 }
                 response = requests.get(url=do_Lottery_url, headers=do_Lottery_headers, verify=False)
                 result = response.json()
-                print ("do_Lottery#抽奖活动")
-                print(result)
+                print ("do_Lottery#抽奖活动\n"+f'{result}')
                 code = result['code']
                 if code == 20003:
                     res = f"抽奖次数已达上限，跳出抽奖环节"
@@ -256,19 +256,24 @@ class DIDI:
         except Exception as e:
             print (e)
 
-    
     def main(self):
         msg_all = ""
         i = 1
         for check_item in self.check_items:
             token = check_item.get("token")
-            lottery_lid = check_item.get("lottery_lid")
+            xpsid = self.get_xpsid()
             s_url = self.get_s_url()
             url_id = self.get_url (s_url=s_url)
             numb,id,day = self.get_id(url_id=url_id)
+            sid = self.get_s(xpsid=xpsid)
+            print (f'获取到的s字段值为:{sid}')
+            hashids = Hashids(salt='o2fXhV')
+            hashid = hashids.decode(sid)[0]
+            lottery_lid = self.get_lid(hashid=hashid, sid=sid)
+            print (f'获取到的lottery_lid值为:{lottery_lid}')
             msg = (
                 f"账号 {i}\n------ 滴滴签到开始------\n"
-                + self.get_activity_info(token=token,day=day,numb=numb)
+                + self.do_sign(token=token,day=day,numb=numb)
                 + "\n"
                 + self.reward(token=token,day=day,numb=numb,id=id)
                 + "\n"
@@ -276,7 +281,7 @@ class DIDI:
                 + "\n------ 滴滴签到结束------"
             )
             i += 1
-            msg_all += msg + "\n\n"     
+            msg_all += msg + "\n\n"
         return msg_all            
 
 if __name__ == "__main__":
